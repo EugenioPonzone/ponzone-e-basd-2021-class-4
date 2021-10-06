7661d514e1346976a474ad37279023855fc15c55
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-10-06 15:22:33 -0300

    excercise01.js done

---
+++ excercise01.js
@@ -0,0 +1,8 @@
+var firstVar = 1;
+var secondVar = 2;
+
+var thirdVar = firstVar + secondVar;
+
+console.log("Excercise 01 FirstVar: "+firstVar);
+console.log("Excercise 01 SecondVar: "+secondVar);
+console.log("Excercise 01 Result: "+thirdVar);
