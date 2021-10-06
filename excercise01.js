c5f8af62906fa3fc76da738a2abf1d8659ad331e
Eugenio Ponzone <eugenioponzone@gmail.com>
2021-10-06 18:31:00 -0300

    excercise01 format corrected

--- excercise01.js
+++ excercise01.js
@@ -4,12 +4,12 @@ var secondVarA = 2;
 
 var thirdVarA = firstVarA + secondVarA;
 
-console.log("Excercise 01A FirstVar: "+firstVarA);
-console.log("Excercise 01A SecondVar: "+secondVarA);
-console.log("Excercise 01A Result: "+thirdVarA);
+console.log("Excercise 01A FirstVar: ", firstVarA);
+console.log("Excercise 01A SecondVar: ", secondVarA);
+console.log("Excercise 01A Result: ", thirdVarA);
 
 //
-console.log("--------------------------------------------")
+console.log("--------------------------------------------");
 
 // EXCERCISE 01B
 
@@ -18,18 +18,18 @@ var secondVarB = "Ponzone";
 
 var thirdVarB = firstVarB + secondVarB;
 
-console.log("Excercise 01B FirstVar: "+firstVarB);
-console.log("Excercise 01B SecondVar: "+secondVarB);
-console.log("Excercise 01B Result: "+thirdVarB);
+console.log("Excercise 01B FirstVar: " + firstVarB);
+console.log("Excercise 01B SecondVar: " + secondVarB);
+console.log("Excercise 01B Result: " + thirdVarB);
 
 //
-console.log("--------------------------------------------")
+console.log("--------------------------------------------");
 
 // EXCERCISE 01C
 var firstVarC = "Eugenio";
 var secondVarC = "Ponzone";
 
-var thirdVarC = firstVarC.length + secondVarC.length
-console.log("Excercise 01C FirstVar: "+firstVarC+"\t Length:"+firstVarC.length);
-console.log("Excercise 01C SecondVar: "+secondVarC+"\t Length:"+secondVarC.length);
-console.log("Excercise 01C Result: "+thirdVarC);
+var thirdVarC = firstVarC.length + secondVarC.length;
+console.log("Excercise 01C FirstVar: " + firstVarC + "\t Length:" + firstVarC.length);
+console.log("Excercise 01C SecondVar: " + secondVarC + "\t Length:" + secondVarC.length);
+console.log("Excercise 01C Result: " + thirdVarC);
